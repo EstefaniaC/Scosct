8f840aad295d233a40a84e24b1d1a2b17c4d0ede
EstefaniaC <estefaniacervantes91@gmail.com>
2016-08-05 17:18:22 -0500

    los 3 estados visualizados

--- apps/revision/migrations/0001_initial.py
+++ apps/revision/migrations/0001_initial.py
@@ -7,14 +7,20 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('registro', '0002_auto_20160726_2235'),
+        ('registro', '0003_remove_registro_numerofolio'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Revision3',
+            name='Revision',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nombreRevisor1', models.CharField(max_length=100, null=True, blank=True)),
+                ('estado1', models.CharField(max_length=30, null=True, blank=True)),
+                ('observacion1', models.TextField(max_length=500, null=True, blank=True)),
+                ('nombreRevisor2', models.CharField(max_length=100, null=True, blank=True)),
+                ('estado2', models.CharField(max_length=30, null=True, blank=True)),
+                ('observacion2', models.TextField(max_length=500, null=True, blank=True)),
                 ('nombreRevisor3', models.CharField(max_length=100, null=True, blank=True)),
                 ('estado3', models.CharField(max_length=30, null=True, blank=True)),
                 ('observacion3', models.TextField(max_length=500, null=True, blank=True)),
